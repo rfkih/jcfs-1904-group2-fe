bf1de557fd82103c67f0493d82a2cf827cbbb7a9
rfkih <rifkihidayat23@yahoo.com>
2022-04-08 17:31:14 +0700

    get transaction detail

--- src/pages/HomeAdmin/component/SalesReport/UsersTransaction/TransactionDetail/TransactionDetail.jsx
+++ src/pages/HomeAdmin/component/SalesReport/UsersTransaction/TransactionDetail/TransactionDetail.jsx
@@ -1,10 +1,13 @@
 import React, { useState , useEffect} from 'react'
 import axios from '../../../../../../utils/axios'
 import { useParams } from "react-router-dom";
+import { Typography,Container, Grid, Card, CardContent,InputBase, Input, IconButton,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core'
 
 function TransactionDetail() {
     const params = useParams();
     const [ listProduct, setlistProduct] = useState({})
+    const [ transactionDetail, setTransactionDetail] = useState({})
+    const [ userDetail, setUserDetail] = useState ({})
 
 
 
@@ -19,11 +22,30 @@ function TransactionDetail() {
           });
       }, []);
 
-      console.log(listProduct);
+
+      const fetchTransactionDetail = async () => {
+        try {
+            const res = await axios.get(`/transaction/${params.transactionId}`, {params: {id: params.transactionId}});
+            const  {data} = res
+            setTransactionDetail(data);
+
+            
+        } catch (error) {
+            console.log(alert(error.message));
+        }
+    };
+
+    useEffect(() => {
+        fetchTransactionDetail();
+    }, [])
+
+   
 
 
   return (
-    <div>TransactionDetail</div>
+    <Container>
+            <Typography>Test </Typography>
+    </Container>
   )
 }
 
