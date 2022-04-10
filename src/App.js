afef2f5187fef7ca03c2b4c575d1402c3d8aa0de
rfkih <rifkihidayat23@yahoo.com>
2022-04-10 11:07:35 +0700

    navbar test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Grid, Box, Stack } from '@material-ui/core';
+import {Brightness2Icon} from '@material-ui/icons'
 import Navbar from './component/Navbar/Navbar'
 import HomeUser from './pages/HomeUser/HomeUser'
 import HomeAdmin from './pages/HomeAdmin/HomeAdmin';
@@ -10,13 +12,15 @@ import ItemSold from './pages/HomeAdmin/component/SalesReport/ItemSold/ItemSold'
 import UsersTransaction from './pages/HomeAdmin/component/SalesReport/UsersTransaction/UsersTransaction';
 import TransactionDetail from './pages/HomeAdmin/component/SalesReport/UsersTransaction/TransactionDetail/TransactionDetail';
 import UserDetail from './pages/HomeAdmin/component/SalesReport/UsersTransaction/UserDetail/UserDetail';
-
+import NavbarAdmin from './pages/HomeAdmin/component/Sidebar/NavbarAdmin';
 function App () {
 
     return (
       <Router>
-      <div>
-  
+
+
+      <NavbarAdmin/>
+     
         <Routes>
           <Route path="/" element={<HomeUser/>} />
           <Route path="homeadmin" element={<HomeAdmin/>}/>
@@ -27,7 +31,7 @@ function App () {
           <Route path={`transactiondetails/:transactionId`} element={<TransactionDetail/>}/>
           <Route path={`usertransaction/:userId`} element={<UserDetail/>}/>
         </Routes>
-      </div>
+      
     </Router>
     )
 }
