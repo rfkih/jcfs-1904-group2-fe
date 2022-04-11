802d181abacaebdeb657e3634f02fcaa1814a220
rfkih <rifkihidayat23@yahoo.com>
2022-04-11 09:57:29 +0700

    start pagination

--- src/pages/HomeAdmin/component/PendingOrder/OrderDetail/OrderDetail.jsx
+++ src/pages/HomeAdmin/component/PendingOrder/OrderDetail/OrderDetail.jsx
@@ -1,9 +1,10 @@
 import React,{useState, useEffect} from 'react'
 import useStyles from './styles'
-import { Typography,Container, Grid, Card, CardContent,InputBase, TextField, Box, Input, IconButton,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core'
+import { Typography,Container, Grid, Card, CardMedia, CardContent,InputBase, TextField, Box, Input, IconButton,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core'
 import axios from '../../../../../utils/axios'
 import { useParams } from "react-router-dom";
 import { set } from 'date-fns/esm';
+import { Link } from 'react-router-dom'
 
 function OrderDetail() {
     const classes = useStyles();
@@ -32,7 +33,28 @@ function OrderDetail() {
   return (
       <Container>
           <div className={classes.toolbar}/>
-            <Typography>Order details</Typography>
+            <Paper>
+                <Card>
+                    <CardMedia
+                        component="img"
+                        alt="Doctor Prescription"
+                        height="400"
+                        image={order.image}
+                    >
+
+                    </CardMedia>
+                        <Typography variant="h6" component="div">Notes</Typography>
+                        <Typography variant="body1">{order.notes}</Typography>
+                    <CardActions>
+                        <Button>Approved</Button>
+                        <Button>Rejected</Button>
+                        <Button> Open Image</Button>
+                    </CardActions>
+
+                </Card>
+
+
+            </Paper>
       </Container>
     
   )
