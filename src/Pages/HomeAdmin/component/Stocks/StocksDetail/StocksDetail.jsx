da416f898f3b91add08bf281fce159fa6fb2529b
rfkih <rifkihidayat23@yahoo.com>
2022-04-18 16:16:43 +0700

    monday

--- src/Pages/HomeAdmin/component/Stocks/StocksDetail/StocksDetail.jsx
+++ src/Pages/HomeAdmin/component/Stocks/StocksDetail/StocksDetail.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import useStyles from './style'
-import { Typography,Container, Grid, Card, CardContent,InputBase, TextField, Box, Input, IconButton,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core'
+import { Typography,Container, Grid, Card, CardMedia, CardContent,InputBase, TextField, Box, Input, IconButton,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core'
 
 
 function StocksDetail() {
     const classes = useStyles();
   return (
     <Container>
-        <div className={classes.toolbar}/>
-        <Typography>
-            WOhyasdfoiha
-        </Typography>
+        <Paper className={classes.content}>
+            <Card className={classes.card}>
+            <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={3}>
+                                <Typography>Product Id</Typography>
+                                <Typography>Product Name </Typography>
+                                <Typography>Product Category </Typography>
+                                <Typography>Product Description</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography>: </Typography>
+                                <Typography>: </Typography>
+                                <Typography>: </Typography>
+                                <Typography>: </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardMedia
+                            component="img"
+                            height = "200"
+                          
+                        />
+                    </Grid>
+                </Grid>
+
+            </Card>
+
+        </Paper>
     </Container>
   )
 }
--- src/Pages/HomeAdmin/component/Stocks/StocksDetail/style.js
+++ src/Pages/HomeAdmin/component/Stocks/StocksDetail/style.js
@@ -7,6 +7,16 @@ export default makeStyles( (theme) => {
     return {
 
         toolbar: theme.mixins.toolbar,
+        content: {
+            flexGrow: 1,
+            backgroundColor: theme.palette.background.default,
+            marginTop: theme.spacing(3),
+            padding: theme.spacing(6),
+          },
+        card: {
+            padding: theme.spacing(2)
+        }
+        
         
     }
 
