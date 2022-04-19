5350b0f258a711b5e81a31cb03a089fb66317d76
rfkih <94028508+rfkih@users.noreply.github.com>
2022-04-19 17:17:01 +0700

    Merapikan folder yang tabrakan(#9)
    * first commit
    
    * monday finished
    
    * category
    
    * checkpoint
    
    * add product
    
    * tuesday
    
    * wednesday
    
    * wednesday night
    
    * early morning
    
    * thursday
    
    * berhasil upload
    
    * new folder structure
    
    * admin
    
    * berhasil soft delete
    
    * edit product picture
    
    * friday night
    
    * get product qty
    
    * get detailed product qty
    
    * update stock successfull
    
    * berhasil undelete
    
    * finishing
    
    * finishing 2
    
    * container
    
    * category selector
    
    * finished
    
    * material ui conversion
    
    * material ui migration completed
    
    * checpoint
    
    * transaction
    
    * monday night
    
    * chart berhasil
    
    * chart finished
    
    * category backend
    
    * search bar
    
    * wanted to save
    
    * master
    
    * table successs
    
    * category fetch
    
    * table finished
    
    * finishes
    
    * success
    
    * thursday
    
    * month name success
    
    * search button
    
    * commit
    
    * year
    
    * transaction status
    
    * end of friday
    
    * get product
    
    * get transaction detail
    
    * checkpoint
    
    * finish
    
    * search
    
    * sorthanlder
    
    * user detail
    
    * date time
    
    * navbar test
    
    * rapihin
    
    * test
    
    * sunday night
    
    * start pagination
    
    * pagination success
    
    * pagination admint
    
    * dleted product pagination
    
    * file upload fixing
    
    * checkpoint
    
    * bug fixed
    
    * revenue completed
    
    * key
    
    * rejected
    
    * home user finished
    
    * admin keyword finished
    
    * before pull request
    
    * delete env
    
    * test
    
    * merging
    
    * repaired
    
    * remake
    
    * product
    
    * git push origin master
    
    * refinement
    
    * refinement
    
    * test
    
    * berhasil
    
    * user search
    
    * init
    
    * stocks
    
    * filter by date
    
    * year
    
    * date
    
    * master
    
    * date complete
    
    * wow
    
    * train
    
    * sebelum ubah
    
    * berhasil
    
    * refine
    
    * transaction complete
    
    * users
    
    * berhasil
    
    * master
    
    * master
    
    * master
    
    * on cancel
    
    * upload photo refinement
    
    * sunday night
    
    * data logging
    
    * fixin
    
    * wow
    
    * berhasil
    
    * fetch products
    
    * wohoo
    
    * backup
    
    * monday
    
    * product detail
    
    * checkpoint
    
    * wow
    
    * stock detail
    
    * backup
    
    * bug fixed
    
    * useSelector
    
    * test
    
    * test
    
    * delete file
    
    * dlete
    
    * tet
    
    * dd

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { keepLoginAction } from "./store/actions";
-import Login from "./Pages/Login";
+import Login from "./pages/Login";
 import Navigation from "./components/Navigation/index";
-import Register from "./Pages/Register";
-import Forgotpass from "./Pages/Login/forgotpass";
-import EditProfile from "./Pages/EditProfile";
-import ProfilePicture from "./Pages/ProfilePicture";
+import Register from "./pages/Register";
+import Forgotpass from "./pages/Login/forgotpass";
+import EditProfile from "./pages/EditProfile";
+import ProfilePicture from "./pages/ProfilePicture"
 
 import { makeStyles } from "@material-ui/core/styles";
-import HomeUser from "./Pages/HomeUser/HomeUser";
-import HomeAdmin from "./Pages/HomeAdmin/HomeAdmin";
-import ProductDetail from "./Pages/HomeUser/components/Products/ProductDetail/ProductDetail";
-import EditDetailProduct from "./Pages/HomeAdmin/component/ProductsAdmin/EditDetailProduct/EditDetailProduct";
-import ItemSold from "./Pages/HomeAdmin/component/SalesReport/ItemSold/ItemSold";
-import UsersTransaction from "./Pages/HomeAdmin/component/SalesReport/UsersTransaction/UsersTransaction";
-import TransactionDetail from "./Pages/HomeAdmin/component/SalesReport/UsersTransaction/TransactionDetail/TransactionDetail";
-import UserDetail from "./Pages/HomeAdmin/component/SalesReport/UsersTransaction/UserDetail/UserDetail";
-import DrawerBar from "./Pages/HomeAdmin/component/Sidebar/Sidebar";
-import SalesReport from "./Pages/HomeAdmin/component/SalesReport/SalesReport";
-import PendingOrder from "./Pages/HomeAdmin/component/PendingOrder/PendingOrder";
-import OrderDetail from "./Pages/HomeAdmin/component/PendingOrder/OrderDetail/OrderDetail";
-import ItemSoldDetail from "./Pages/HomeAdmin/component/SalesReport/ItemSold/ItemSoldDetail/ItemSoldDetail";
-import Stocks from "./Pages/HomeAdmin/component/Stocks/Stocks";
+import HomeUser from "./pages/HomeUser/HomeUser";
+import HomeAdmin from "./pages/HomeAdmin/HomeAdmin";
+import ProductDetail from "./pages/HomeUser/components/Products/ProductDetail/ProductDetail";
+import EditDetailProduct from "./pages/HomeAdmin/component/ProductsAdmin/EditDetailProduct/EditDetailProduct";
+import ItemSold from "./pages/HomeAdmin/component/SalesReport/ItemSold/ItemSold";
+import UsersTransaction from "./pages/HomeAdmin/component/SalesReport/UsersTransaction/UsersTransaction";
+import TransactionDetail from "./pages/HomeAdmin/component/SalesReport/UsersTransaction/TransactionDetail/TransactionDetail";
+import UserDetail from "./pages/HomeAdmin/component/SalesReport/UsersTransaction/UserDetail/UserDetail";
+import DrawerBar from "./pages/HomeAdmin/component/Sidebar/Sidebar";
+import SalesReport from "./pages/HomeAdmin/component/SalesReport/SalesReport";
+import PendingOrder from "./pages/HomeAdmin/component/PendingOrder/PendingOrder";
+import OrderDetail from "./pages/HomeAdmin/component/PendingOrder/OrderDetail/OrderDetail";
+import ItemSoldDetail from './pages/HomeAdmin/component/SalesReport/ItemSold/ItemSoldDetail/ItemSoldDetail'
+import Stocks from "./pages/HomeAdmin/component/Stocks/Stocks";
+import StocksDetail from "./pages/HomeAdmin/component/Stocks/StocksDetail/StocksDetail";
 
 const useStyles = makeStyles({
   page: {
@@ -37,9 +38,7 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles();
-
-  const [role, setRole] = useState("users");
-
+  const [role, setRole] = useState("");
   const [isLocalStorageChecked, setIsLocalStorageChecked] = useState(false);
   const dispatch = useDispatch();
 
@@ -49,7 +48,7 @@ function App() {
       const userData = JSON.parse(userLocalStorage);
 
       const { id, username, role, tokens } = userData;
-
+      setRole(role)
       dispatch(keepLoginAction({ id, username, role, tokens }));
     }
 
@@ -68,11 +67,10 @@ function App() {
               <Routes>
                 <Route path="/" element={<HomeUser />} />
                 <Route path="homeadmin" element={<HomeAdmin />} />
-
                 <Route path="stocks" element={<Stocks />} />
-
                 <Route path="financial" element={<SalesReport />} />
                 <Route path="orders" element={<PendingOrder />} />
+                <Route path="stocks/:productId" element={<StocksDetail />} />
                 <Route path={`orders/:orderId`} element={<OrderDetail />} />
                 <Route
                   path={`products/:productId`}
@@ -83,6 +81,7 @@ function App() {
                   element={<EditDetailProduct />}
                 />
                 <Route path={`itemsold`} element={<ItemSold />} />
+                <Route path={`itemsold/product/:productId`} element={<ItemSoldDetail />} />
                 <Route
                   path={`itemsold/product/:productId`}
                   element={<ItemSoldDetail />}
@@ -110,7 +109,10 @@ function App() {
             <div className={classes.page}>
               <Routes>
                 <Route path="/" element={<HomeUser />} />
-
+                <Route
+                  path={`products/:productId`}
+                  element={<ProductDetail />}
+                />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/reset-password" element={<Forgotpass />} />
@@ -130,4 +132,4 @@ function App() {
   }
 }
 
-export default App;
+export default App
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,3 +1,45 @@
+<<<<<<< HEAD
+import React from 'react'
+import {AppBar, Toolbar, Typography, IconButton, Badge, MenuItem, Menu,} from '@material-ui/core';
+import { Link, useLocation } from 'react-router-dom'
+import { ShopingCart, ShoppingCart } from '@material-ui/icons';
+
+
+import logo from '../../assets/logo.png'
+
+
+import useStyles from './styles';
+
+function Navbar() {
+    const classes = useStyles();
+    const location = useLocation();
+
+  return (
+    <AppBar position="fixed" className={classes.AppBar} color="inherit">
+        <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+            <img src={logo} alt="pharmacy" height="25px" className={classes.image}/>
+                Pharmacy
+            </Typography>
+            <div className={classes.grow}/>
+            {location.pathname === '/' && (
+                  <div className={classes.button}>
+                  <IconButton component={Link} to="/cart" aria-label="Show cat items" color="inherit">
+                      <Badge badgeContent={2} color="secondary">
+                        <ShoppingCart/>
+                      </Badge>
+                  </IconButton>
+              </div>
+                ) 
+              }
+
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
+=======
 import React from "react";
 import {
   AppBar,
@@ -52,3 +94,4 @@ function Navbar() {
 }
 
 export default Navbar;
+>>>>>>> 09d7affdbf8d42ae1c64453c2f463b055e1acc2c
