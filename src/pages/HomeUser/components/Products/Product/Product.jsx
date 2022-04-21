788ba3201e8cc564638cc230505b855748db06fc
rfkih <rifkihidayat23@yahoo.com>
2022-04-21 09:32:55 +0700

    dummy image berhasil

--- src/pages/HomeUser/components/Products/Product/Product.jsx
+++ src/pages/HomeUser/components/Products/Product/Product.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton, useTheme} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
-
-
 import useStyles from './styles'
 
+
+
 function Product({product}) {
+    const classes = useStyles();
+    const [image ,setImage] = useState('https://pharmanewsintel.com/images/site/article_headers/_normal/Medicine.png')
     const alternateimage = "https://pharmanewsintel.com/images/site/article_headers/_normal/Medicine.png"
     
-    const classes = useStyles();
+  
+   
+
+    useEffect(() => {
+        if (product.productIMG) {
+            setImage(product.productIMG)
+        }
+    },[])
 
 
   return (
     <Card sx={{ maxWidth: 300 }}>
-        <CardMedia component="img"  height="140" image={product.productIMG} alt={alternateimage}/>
+        <CardMedia component="img"  height="140" image={image} alt={alternateimage}/>
         <CardContent>
                 <Typography variant="body2">
                        Rp.{product.price}
